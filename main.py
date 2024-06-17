f30e8c0e3268dc010b402cc2d3f1e33c934834e8
noahtolsen <noah.t.olsen@gmail.com>
2024-06-17 18:07:59 -0400

    removing debugging info

--- main.py
+++ main.py
@@ -18,20 +18,6 @@ load_dotenv()
 # Ensure the AWS region is set
 os.environ['AWS_REGION'] = 'us-east-1'  # Replace 'us-east-1' with your desired region
 
-# Debugging: Print environment variables to ensure AWS_REGION is set correctly
-print("AWS_REGION:", os.getenv('AWS_REGION'))
-
-# Function to retrieve the current credentials and log them for debugging
-def log_current_credentials(session):
-    credentials = session.get_credentials()
-    if credentials:
-        credentials = credentials.get_frozen_credentials()
-        print(f"AWS Access Key: {credentials.access_key}")
-        print(f"AWS Secret Key: {credentials.secret_key[:4]}...")  # Hide most of the secret key for security
-        print(f"AWS Session Token: {credentials.token}")
-    else:
-        print("No credentials found")
-
 # Create a botocore session and specify the region
 try:
     botocore_session = botocore.session.get_session()
@@ -39,7 +25,6 @@ try:
     cache_config = SecretCacheConfig()
     cache = SecretCache(config=cache_config, client=client)
     secret = json.loads(cache.get_secret_string('gmgf_secrets'))
-    print("Successfully retrieved secrets from Secrets Manager")
 except Exception as e:
     st.error(f"Error accessing Secrets Manager: {e}")
     st.stop()
@@ -61,19 +46,16 @@ if not is_logged_in:
     st.stop()
 
 def logout():
-    print("Logout in example")
     authenticator.logout()
 
 # Amazon Bedrock - settings
 try:
     # Create a Boto3 session without specifying a profile
     session = boto3.Session(region_name=os.getenv('AWS_REGION'))
-    log_current_credentials(session)  # Log the current credentials for debugging
     bedrock_runtime = session.client(
         service_name="bedrock-runtime",
         region_name=os.getenv('AWS_REGION'),
     )
-    print("Successfully created Bedrock runtime client")
 except Exception as e:
     st.error(f"Error creating AWS session: {e}")
     st.stop()
@@ -108,7 +90,6 @@ try:
         aws_session_token=credentials.token,
         retrieval_config={"vectorSearchConfiguration": {"numberOfResults": 4}},  # Retrieve multiple passages
     )
-    print("Successfully created AmazonKnowledgeBasesRetriever")
 except Exception as e:
     st.error(f"Error creating AmazonKnowledgeBasesRetriever: {e}")
     st.stop()
